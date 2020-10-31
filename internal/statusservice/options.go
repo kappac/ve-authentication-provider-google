c7fd89094419efa2c3ce7f2f4a8d6653a8af57ee
Constantine Povietkin <povetkin.k@gmail.com>
2020-10-31 14:52:16 +0200

    statusservice.Service -> statusservice.StatusService

--- internal/statusservice/options.go
+++ internal/statusservice/options.go
@@ -1,18 +1,18 @@
 package statusservice
 
 // NewOption is an option updater for the constructor
-type NewOption func(s *service)
+type NewOption func(s *statusService)
 
 // WithAddress sets an address service will listen at.
 func WithAddress(a string) NewOption {
-	return func(s *service) {
+	return func(s *statusService) {
 		s.address = a
 	}
 }
 
 // WithEndpointSource sets a source for an apropriate Probe.
 func WithEndpointSource(p Probe, src SourceSubscriber) NewOption {
-	return func(s *service) {
+	return func(s *statusService) {
 		s.sources[p] = src
 	}
 }
